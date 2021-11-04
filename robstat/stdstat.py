56afb2b5386b3688264b183848cfccf16540e916
matyasmolnar <mdm49@cam.ac.uk>
2021-11-04 16:50:32 +0000

    for rsc_average - if flagged in Re or Im when MAD-clipping, flag both components; implement rsc_average_astropy implementation

--- robstat/stdstat.py
+++ robstat/stdstat.py
@@ -1,7 +1,10 @@
 """Standard statistical techniques"""
 
 
+import warnings
+
 import numpy as np
+from astropy.stats import sigma_clip, mad_std
 
 from . import utils
 
@@ -95,7 +98,7 @@ def mad_clip(data, flags=None, sigma=4.0, axis=0, min_N=4, verbose=False):
 
 def rsc_avg(data, stat, flags=None, sigma=4.0, axis=0, min_N=4, verbose=False):
     """
-    Mean of data after robust sigma clipping.
+    Location of data after robust sigma clipping.
 
     Mimics HERA LST-binning data averaging.
 
@@ -145,7 +148,9 @@ def rsc_avg(data, stat, flags=None, sigma=4.0, axis=0, min_N=4, verbose=False):
                                   min_N=min_N)
         utils.echo('{} data points MAD-clipped.'.format(real_f.sum()+imag_f.sum()-flg_count), \
                    verbose=verbose)
-        return np_stat(real_d, axis=axis) + 1j * np_stat(imag_d, axis=axis)
+        mc_data = real_d + 1j * imag_d
+        mc_data[real_f + imag_f] *= np.nan # flag data if flagged in either Re or Imag component
+        return np_stat(mc_data, axis=axis)
 
     else:
         d, f = mad_clip(data, flags=flags, sigma=sigma, axis=axis, min_N=min_N, \
@@ -158,3 +163,79 @@ rsc_mean = lambda data, flags=None, sigma=4.0, axis=0, min_N=4, verbose=False: \
 
 rsc_median = lambda data, flags=None, sigma=4.0, axis=0, min_N=4, verbose=False: \
     rsc_avg(data, 'median', flags, sigma, axis, min_N, verbose)
+
+
+def rsc_avg_astropy(data, stat, flags=None, sigma=4.0, axis=0, min_N=4, verbose=False):
+    """
+    Location of data after robust sigma clipping.
+
+    Astropy implementation.
+
+    Args:
+        data (ndarray): input data.
+        stat (str): statistic to use for averaging ('mean', 'median').
+        flags (ndarray): existing boolean flags for data array. True if flagged.
+        sigma (ndarray): sigma threshold to cut above.
+        axis (int): axis of array to perform mean over.
+        min_N (int): minimum length of array to sigma clip, below which no sigma
+        clipping is performed.
+        verbose (bool): print number of MAD-clipped data points.
+
+    Returns:
+        robust sigma clipped mean (ndarray).
+    """
+    if stat in ('mean', 'median'):
+        np_stat = getattr(np, 'nan{}'.format(stat))
+    else:
+        raise ValueError('stat must be either "mean" or "median".')
+
+    iscomplex = np.iscomplexobj(data)
+    if iscomplex:
+        # ensure imag entries are also nan'd
+        if np.isnan(data).any():
+            data[np.isnan(data)] *= np.nan
+
+    if flags is not None:
+        data[flags] *= np.nan
+
+    if np.isnan(data).all():
+        utils.echo('All-nan slice encountered - returning nan.', verbose=verbose)
+        if data.ndim == 1:
+            if iscomplex:
+                return np.nan + 1j*np.nan
+            else:
+                return np.nan
+        else:
+            rtn_arr_shape = np.delete(data.shape, axis)
+            return np.empty(rtn_arr_shape, dtype=data.dtype) * np.nan
+
+    # pass ignore_nan=True argument to astropy mad_std
+    def mad_std_(d, axis=None):
+        return mad_std(d, axis=axis, func=None, ignore_nan=True)
+
+    # ensure array passes min_N criteria:
+    if isinstance(axis, int):
+        data_N = data.shape[axis]
+    else:
+        data_N = np.prod([ax for i, ax in enumerate(data.shape) if i not in axis])
+    if data_N < min_N:
+        utils.echo('No sigma clipping performed as length of array for specified axis ' \
+                   '< min_N.', verbose=verbose)
+        return np_stat(data, axis=axis)
+
+    with warnings.catch_warnings():
+        warnings.filterwarnings('ignore', message='Input data contains invalid values')
+        if iscomplex:
+            sc_re = sigma_clip(data.real, cenfunc=np.nanmedian, stdfunc=mad_std_, \
+                               sigma=sigma, maxiters=1, axis=axis)
+            sc_im = sigma_clip(data.imag, cenfunc=np.nanmedian, stdfunc=mad_std_, \
+                               sigma=sigma, maxiters=1, axis=axis)
+            sc = sc_re + 1j*sc_im
+            sc.set_fill_value(value=np.nan+1j*np.nan)
+
+        else:
+            sc = sigma_clip(data, cenfunc=np.nanmedian, stdfunc=mad_std_, \
+                            sigma=sigma, maxiters=1, axis=axis)
+            sc.set_fill_value(value=np.nan)
+
+    return np_stat(sc.filled(), axis=axis)
