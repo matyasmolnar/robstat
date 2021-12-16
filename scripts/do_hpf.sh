fce293cf633e8bad16e6ee2159576c8b6a55c6df
matyasmolnar <mdm49@cam.ac.uk>
2021-12-16 04:29:24 -0700

    change working directory

--- scripts/do_hpf.sh
+++ scripts/do_hpf.sh
@@ -5,14 +5,14 @@
 # interpreted as command line arguments to sbatch.
 # Don't put any commands before the #SBATCH directives or they will not work.
 #
-#SBATCH --export=ALL                                  # Export all environment variables to job
-#SBATCH --partition=hera                              # Specify partition on which to run job
-#SBATCH --mem=100G                                    # Amount of memory needed by the whole job
-#SBATCH -D /lustre/aoc/projects/hera/mmolnar/robstat  # Working directory
-#SBATCH --mail-type=BEGIN,END,FAIL                    # Send email on begin, end, and fail of job
-#SBATCH --nodes=1                                     # Request 1 node
-#SBATCH --ntasks-per-node=8                           # Request 8 cores
-#SBATCH --time=48:00:00                               # Request 48 hours, 0 minutes and 0 seconds.
+#SBATCH --export=ALL                                          # Export all environment variables to job
+#SBATCH --partition=hera                                      # Specify partition on which to run job
+#SBATCH --mem=100G                                            # Amount of memory needed by the whole job
+#SBATCH -D /lustre/aoc/projects/hera/mmolnar/robstat/scripts  # Working directory
+#SBATCH --mail-type=BEGIN,END,FAIL                            # Send email on begin, end, and fail of job
+#SBATCH --nodes=1                                             # Request 1 node
+#SBATCH --ntasks-per-node=8                                   # Request 8 cores
+#SBATCH --time=48:00:00                                       # Request 48 hours, 0 minutes and 0 seconds.
 #SBATCH --output=hpf.out
 
 source ~/.bashrc
